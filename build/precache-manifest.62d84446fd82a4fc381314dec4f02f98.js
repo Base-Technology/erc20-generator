c1efa4f29562aae0cba89de646e614a96bcc1875
doctor <546053621@qq.com>
2023-04-22 10:58:43 +0800

    save

---
+++ build/precache-manifest.62d84446fd82a4fc381314dec4f02f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe06a601594a9d849111",
+    "url": "/static/css/main.717b0276.chunk.css"
+  },
+  {
+    "revision": "fe06a601594a9d849111",
+    "url": "/static/js/main.da1c0550.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "376775dcb6ae0626ed67",
+    "url": "/static/css/2.58af2e6a.chunk.css"
+  },
+  {
+    "revision": "376775dcb6ae0626ed67",
+    "url": "/static/js/2.bd0c5722.chunk.js"
+  },
+  {
+    "revision": "23984b71e87242b55b84bf2f77ad8f24",
+    "url": "/index.html"
+  }
+];
