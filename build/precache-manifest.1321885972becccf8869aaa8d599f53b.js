1ece0ebcb8685a802e0332501f24be4d8775d597
krabat-h <lugh60@gmail.com>
2023-04-23 20:58:12 +0800

    add airdrop dist

---
+++ build/precache-manifest.1321885972becccf8869aaa8d599f53b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f259cc2b1c178aaa87c3",
+    "url": "/static/css/main.9ac2a58e.chunk.css"
+  },
+  {
+    "revision": "f259cc2b1c178aaa87c3",
+    "url": "/static/js/main.1453a4a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "085c8ecbb00cf91fc34a",
+    "url": "/static/css/2.4b84c20c.chunk.css"
+  },
+  {
+    "revision": "085c8ecbb00cf91fc34a",
+    "url": "/static/js/2.b1680998.chunk.js"
+  },
+  {
+    "revision": "f59095f112087030999ce4a7cdd3ae2d",
+    "url": "/index.html"
+  }
+];
