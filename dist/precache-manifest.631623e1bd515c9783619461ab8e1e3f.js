4fbd8ae20979ce9b38f281c25dee65b14c7b81d7
krabat-h <lugh60@gmail.com>
2023-06-06 21:25:35 +0800

    fix tax limit error

--- dist/precache-manifest.631623e1bd515c9783619461ab8e1e3f.js
+++ dist/precache-manifest.631623e1bd515c9783619461ab8e1e3f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6677d7a1535b8b87cd3a",
+    "revision": "fef267b623c401c68d54",
     "url": "/static/css/main.9ac2a58e.chunk.css"
   },
   {
-    "revision": "6677d7a1535b8b87cd3a",
-    "url": "/static/js/main.dcc9b3cd.chunk.js"
+    "revision": "fef267b623c401c68d54",
+    "url": "/static/js/main.1cb8a2f8.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.9a26e90a.chunk.js"
   },
   {
-    "revision": "cd5fc25f7e013b405acaee710ac42178",
+    "revision": "d2b6aba995b421bad76046426fde370d",
     "url": "/index.html"
   }
 ];
