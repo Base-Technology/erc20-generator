bb2177449794bbd333d238305a45d7342c8bf278
krabat-h <lugh60@gmail.com>
2023-06-08 23:59:37 +0800

    move build to dist

---
+++ dist/precache-manifest.63fd0f651a7d3da33ff5879b02c698ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c26e104f5b34ef2296d",
+    "url": "/static/css/main.9ac2a58e.chunk.css"
+  },
+  {
+    "revision": "5c26e104f5b34ef2296d",
+    "url": "/static/js/main.bc88a248.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48db0bed6bd4cea94260",
+    "url": "/static/css/2.4b84c20c.chunk.css"
+  },
+  {
+    "revision": "48db0bed6bd4cea94260",
+    "url": "/static/js/2.65a5297e.chunk.js"
+  },
+  {
+    "revision": "deb83b4c7a5b4bf3eb11426a2240ca5d",
+    "url": "/index.html"
+  }
+];
